0c31076bdbddac0a2e162ab546a4a0a71a4ba385
Lucas Hodge <lucas@insights.net.au>
2020-11-10 09:42:25 +1100

    Added icu4c@64.2

---
+++ icu4c@64.2.rb
@@ -0,0 +1,37 @@
+class Icu4cAT642 < Formula
+  desc "C/C++ and Java libraries for Unicode and globalization"
+  homepage "https://ssl.icu-project.org/"
+  url "https://ssl.icu-project.org/files/icu4c/64.2/icu4c-64_2-src.tgz"
+  mirror "https://github.com/unicode-org/icu/releases/download/release-64-2/icu4c-64_2-src.tgz"
+  version "64.2"
+  sha256 "627d5d8478e6d96fc8c90fed4851239079a561a6a8b9e48b0892f24e82d31d6c"
+
+  bottle do
+    cellar :any
+    sha256 "e707bf5e3d0189ede7d941d95a417b5dacad3eac99b9a677042464140f12fa1d" => :mojave
+    sha256 "0ac5ee60393d26ec26a915ed957a38a0b2355fe7991f607044edaedd3ff14cc1" => :high_sierra
+    sha256 "dd64c70639ba91fe0c07304c0563681ba97b221e9f9919497b383884b8bb0b65" => :sierra
+  end
+
+  keg_only :provided_by_macos, "macOS provides libicucore.dylib (but nothing else)"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --disable-samples
+      --disable-tests
+      --enable-static
+      --with-library-bits=64
+    ]
+
+    cd "source" do
+      system "./configure", *args
+      system "make"
+      system "make", "install"
+    end
+  end
+
+  test do
+    system "#{bin}/gendict", "--uchars", "/usr/share/dict/words", "dict"
+  end
+end
